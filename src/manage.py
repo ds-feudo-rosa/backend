3dc2e50f8ff6ff2ac4a8a75178de062054d90ca0
Daniel Sousa <95941136+daniel-de-sousa@users.noreply.github.com>
2022-03-09 16:48:02 -0300

    Subindo Manage

--- src/manage.py
+++ src/manage.py
@@ -1,2 +1,9 @@
-print('Hello World!')
+from flask.cli import FlaskGroup
+from app import create_app
 
+
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)
+
+if __name__ == '__main__':
+    cli()
