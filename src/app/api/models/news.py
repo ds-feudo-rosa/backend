84735f2d86bc01e1d7d30b49c5fb8f763f3bc904
Daniel Sousa <95941136+daniel-de-sousa@users.noreply.github.com>
2022-04-09 16:12:30 -0300

    Criação do banco de dados para salvar noticias e salvar os votos

--- src/app/api/models/news.py
+++ src/app/api/models/news.py
@@ -1,19 +1,38 @@
 from app import db
-from flask_login import UserMixin
 
 #criação das tabelas dos usuários, herdando da classe do Model
-class NewsModel(db.Model, UserMixin):
+class NewsModel(db.Model):
     __tablename__ = "news"
 
     #Criação de Colunas no SQL
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(100), nullable=False)
-    text = db.Column(db.String(100), nullable=False, unique=True)
+    title = db.Column(db.String(100), nullable=False, unique=False)
+    text = db.Column(db.String(), nullable=False, unique=False)
 
     #Construtor - Inicializa todos os campos, normalmente é os campos obrigatórios, especificando quando ela for inicializada
-    def __init__(self, text, title):
-        self.text = text
+    def __init__(self, title, text):
         self.title = title
+        self.text = text
+    
+    def get_id(self):
+        return str(self.id)
+    
+    def save(self):
+        db.session.add(self)
+        return db.session.commit()
+
+class VoteModel(db.Model):
+    __tablename__ = "vote"
+
+    #Criação de Colunas no SQL
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    like = db.Column(db.Integer)
+    unlike = db.Column(db.Integer)
+
+    #Construtor - Inicializa todos os campos, normalmente é os campos obrigatórios, especificando quando ela for inicializada
+    def __init__(self, like, unlike):
+        self.like = like
+        self.unlike = unlike
     
     def get_id(self):
         return str(self.id)
---
+++ src/app/api/views/news.py
@@ -0,0 +1,52 @@
+from app import api_restful
+from flask_restful import Resource
+from flask import  request
+from app.api.models.news import NewsModel, VoteModel
+from flask_login import current_user
+
+class NewNotice(Resource):
+    def get(self):
+        return "Ação Invalida"
+    
+    def post(self):
+        if current_user.is_authenticated:
+            data = request.get_json()
+            notice = NewsModel.query.filter_by(text=data['text']).first()
+            if notice:
+                return "Essa noticia já foi cadastrada"
+            else:
+                notice = NewsModel(title=data['title'], text=data['text'])
+                notice.save()
+                votos = VoteModel(like=0, unlike=0)
+                votos.save()
+                return 'Noticia cadastrada com sucesso'
+        else:
+            return 'Faça login antes de publicar uma noticia', 203
+
+class ReturnNotice(Resource):
+    def get(self):
+        return "Ação Inválida"
+    
+    def post(self):
+        data = request.get_json()
+        notice = NewsModel.query.filter_by(id=data['id']).first()
+        votos = VoteModel.query.filter_by(id=data['id']).first()
+        if notice:
+            return ("A noticia é {} Sua quatidade de Votos é {} likes e {} deslikes".format(notice.text, votos.like, votos.unlike))
+
+class VotoNotice(Resource):
+    def post(slef):
+        if current_user.is_authenticated:
+            data = request.get_json()
+            notice = NewsModel.query.filter_by(id=data['id']).first()
+            if notice:
+                votos = VoteModel.query.filter_by(id=data['id']).first()
+                votos.like = votos.like + int(data['likes'])
+                votos.unlike = votos.unlike + int(data['unlikes'])
+                votos.save()
+                return "Votação Salva"
+        
+
+api_restful.add_resource(NewNotice, '/new/', endpoint='news', methods=['GET', 'POST'])
+api_restful.add_resource(ReturnNotice, '/notice/', endpoint='notice', methods=['GET', 'POST'])
+api_restful.add_resource(VotoNotice, '/voto/', endpoint='voto', methods=['GET', 'POST'])
